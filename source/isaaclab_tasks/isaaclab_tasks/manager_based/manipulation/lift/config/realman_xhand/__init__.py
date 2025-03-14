e2462959c526fe61ca56fdc744c21a2ac3ff3efd
LuolinRowling <luolin.rowling@gmail.com>
2025-03-14 18:52:58 +0800

    add lift_realman_config

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/realman_xhand/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+import gymnasium as gym
+import os
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+##
+# Joint Position Control
+##
+print("=========================")
+print(f"{agents.__name__}:rl_games_ppo_cfg.yaml")
+print("=========================")
+gym.register(
+    id="Isaac-Lift-Cube-Realman-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.joint_pos_env_cfg:RealmanCubeLiftEnvCfg",
+        # "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LiftCubePPORunnerCfg",
+        # "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        # "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+    disable_env_checker=True,
+)
+
